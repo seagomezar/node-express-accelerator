46d060df669c4699dbb60d2610033ea735fb1303
Sebastian Gomez <seagomezar@gmail.com>
2022-07-22 10:34:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Test for the codelab this should be snyk!!');
+  res.send('Test for the codelab this should be sny!k!!');
 });
 
 app.get('/health', (req, res) => {
